4e6dd924c5d00fc51b67bd74000a113b9e2316af
khegazy <KareemHegz@gmail.com>
2025-04-27 00:16:36 -0700

    initial test commits

---
+++ tests/analytic_potentials/test_potentials.py
@@ -0,0 +1,22 @@
+import os
+import torch
+import numpy as np
+from popcornn import tools
+from popcornn.run_optimization import optimize_MEP
+
+
+POTENTIALS = ['wolfe_schlegel', 'morse', 'lennard_jones', 'muller_brown', 'schwefel', 'repel', 'harmonic']
+
+def test_potentials():
+    torch.manual_seed(2025)
+    np.random.seed(2025)
+    
+    for pot in POTENTIALS:
+        config_path = os.path.join("configs", f"{pot}.yaml")
+        config = tools.import_run_config(config_path)
+    
+        # Run the optimization
+        final_images, ts_image, optimization_results = optimize_MEP(
+            save_optimization_freq=None, **config
+        )
+test_potentials()
