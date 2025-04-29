93bd8e92297972b8133403fe16fa52bfe68e1a48
khegazy <KareemHegz@gmail.com>
2025-04-28 21:03:25 -0700

    New config directory

--- tests/analytic_potentials/test_potentials.py
+++ tests/analytic_potentials/test_potentials.py
@@ -5,14 +5,17 @@ from popcornn import tools
 from popcornn.run_optimization import optimize_MEP
 
 
-POTENTIALS = ['wolfe_schlegel', 'morse', 'lennard_jones', 'muller_brown', 'schwefel', 'repel', 'harmonic']
+POTENTIALS = ['wolfe_schlegel', 'repel', 'morse']#, 'lennard_jones', 'muller_brown', 'schwefel', 'repel', 'harmonic']
+POTENTIALS = ['morse']#, 'lennard_jones', 'muller_brown', 'schwefel', 'repel', 'harmonic']
 
 def test_potentials():
     torch.manual_seed(2025)
     np.random.seed(2025)
     
     for pot in POTENTIALS:
-        config_path = os.path.join("configs", f"{pot}.yaml")
+        print("STARTING POTENTIAL", pot)
+        #config_path = os.path.join("configs", f"{pot}.yaml")
+        config_path = os.path.join("configs", f"6445_{pot}.yaml")
         config = tools.import_run_config(config_path)
     
         # Run the optimization
