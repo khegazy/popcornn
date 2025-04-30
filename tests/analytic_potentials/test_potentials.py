b716f8adb5734fe29c826bf6a7180ee79d816877
khegazy <KareemHegz@gmail.com>
2025-04-29 17:37:28 -0700

    update with new popcornn class

--- tests/analytic_potentials/test_potentials.py
+++ tests/analytic_potentials/test_potentials.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import numpy as np
+from popcornn import Popcornn
 from popcornn import tools
 from popcornn.run_optimization import optimize_MEP
 
@@ -11,6 +12,7 @@ POTENTIALS = ['morse']#, 'lennard_jones', 'muller_brown', 'schwefel', 'repel', '
 def test_potentials():
     torch.manual_seed(2025)
     np.random.seed(2025)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     
     for pot in POTENTIALS:
         print("STARTING POTENTIAL", pot)
@@ -19,7 +21,6 @@ def test_potentials():
         config = tools.import_run_config(config_path)
     
         # Run the optimization
-        final_images, ts_image, optimization_results = optimize_MEP(
-            save_optimization_freq=None, **config
-        )
+        mep = Popcornn(device=device, **config.get('init_params', {}))
+        final_images, ts_image = mep.run(*config.get('opt_params', []))
 test_potentials()
